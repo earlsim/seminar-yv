3a6dd5c2775304e44a3c3b8dc6e3cea47c6983c5
earlsim <zmnation15rus@mail.com>
2019-10-03 19:41:33 +0300

    '03-10'

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,34 @@
-let fillerEl = document.querySelector('.js-mobile-filters');
-if (fillerEl) {
-    fillerEl.addEventListener('click', function () {
-        let item = document.querySelector('.js-nav');
-        item.classList.add('is-active');
-    },false);
-};
-let closeEl = document.querySelector('.js-close');
-if (closeEl) {
-    closeEl.addEventListener('click', function () {
-        let item = document.querySelector('.js-nav');
-        item.classList.remove('is-active');
-    })
-};
 
-$(function () {
-    lgChem.init();
+//show/hide mobile filters
+$(document).ready(function () {
+
+    var fillerEl = document.querySelector('.js-mobile-filters');
+    if (fillerEl) {
+
+        fillerEl.addEventListener('click', function () {
+            var item = document.querySelector('.js-nav');
+            item.classList.add('is-active');
+        });
+    };
+    var closeEl = document.querySelector('.js-close');
+    if (closeEl) {
+        closeEl.addEventListener('click', function () {
+            var item = document.querySelector('.js-nav');
+            item.classList.remove('is-active');
+        })
+    };
+
+//Datapicker for filters
+    var dateToday = new Date();
+    $( function() {
+        $( "#datepicker" ).datepicker({
+            minDate: dateToday,
+        });
+    } );
+
+    $("body").on("focus", "#datepicker", function(){
+        $(this).datepicker({minDate: dateToday});
+    });
+    $.datepicker.setDefaults( $.datepicker.regional[ "ru" ] );
+
 });
-
-$( function() {
-    $( "#datepicker" ).datepicker();
-} );
